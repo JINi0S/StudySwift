26836e14b463fc190747a4a33b8fa5d672d80d97
jinhee8847 <100195563+jinhee8847@users.noreply.github.com>
2022-03-11 00:15:27 +0900

    Create 1001.A-B.swift

---
+++ 백준/1001.A-B.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HundredJoon
+//
+//  Created by 이진희 on 2022/03/10.
+//
+
+import Foundation
+
+let read = readLine()!
+let str = read.split(separator: " ")
+let result = Int(str[0])! - Int(str[1])!
+print(result)
