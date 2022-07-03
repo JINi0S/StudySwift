0dc065bf2a8e03370bc72dd2d69da4eadf0ed7b1
jinhee8847 <100195563+jinhee8847@users.noreply.github.com>
2022-07-03 19:46:28 +0900

    Add files via upload

---
+++ FC/Project08.Weather/Project08.Weather/WeatherInfomation.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherInfomation.swift
+//  Project08.Weather
+//
+//  Created by 이진희 on 2022/07/02.
+//
+
+import Foundation
+
+struct WeatherInformation: Codable {
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let minTemp: Double
+    let maxTemp: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
+}
---
+++ FC/Project08.Weather/Project08.Weather/errorMessage.swift
@@ -0,0 +1,11 @@
+//
+//  errorMessage.swift
+//  Project08.Weather
+//
+//  Created by 이진희 on 2022/07/02.
+//
+
+import Foundation
+struct ErrorMessage: Codable {
+    let message: String
+}
