8b5ffcf1f7cf38270497036c44f71800d0e7e3a7
jinhee8847 <100195563+jinhee8847@users.noreply.github.com>
2022-07-26 01:44:11 +0900

    Add files via upload

---
+++ FC/Project11.DrinkWaterAlarm/Project11.Drink/Drink/AddAlertViewController.swift
@@ -0,0 +1,24 @@
+//
+//  addAlertViewController.swift
+//  Drink
+//
+//  Created by 이진희 on 2022/07/25.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController{
+    var pickedDate: ((_ date: Date) -> Void)?
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func dismissButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
